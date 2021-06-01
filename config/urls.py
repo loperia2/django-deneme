166485d096783235e83eac7a60bdb42c5b55020c
Güney <gney96@gmail.com>
2021-06-01 08:55:02 +0300

    Templates

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('deneme/', include('deneme.urls'))
+    path('', include('deneme.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
